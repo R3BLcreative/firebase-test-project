53e621e38544c49dc4927fe870f171046020554a
James Cook <james.cook@helloalice.com>
2023-12-15 09:18:11 -0600

    INIT

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCJX7utdLpdUKGwPN27-dCUW6XvOAAH8JI',
+	authDomain: 'test-project-da190.firebaseapp.com',
+	projectId: 'test-project-da190',
+	storageBucket: 'test-project-da190.appspot.com',
+	messagingSenderId: '574727428890',
+	appId: '1:574727428890:web:a00873358ec429859daaa3',
+	measurementId: 'G-LH081QLRWE',
+};
+
+const app = initializeApp(firebaseConfig);
